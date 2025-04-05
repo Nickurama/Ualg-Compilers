55ec14f36528c0306fd4d03c098b6c45ce03e471
luna <rokdoombeast@gmail.com>
2025-04-05 18:10:11 +0100

    project backbone done

---
+++ project/scripts/antlr_compile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd src
+antlr=$(echo ../lib/antlr-*.jar)
+grammar=$1
+grammar_file="$grammar.g4"
+antlr_dir="$grammar"
+
+# rm -rf $(echo $antlr_dir/*)
+
+java -jar $antlr -visitor $grammar_file -o $antlr_dir -package $grammar
+owo_print "done parsing antlr code."
+
+cd ..
+java_compile
---
+++ project/scripts/antlr_parsing_tree.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# requires "grun" from the arch "antlr4" package
+# third argument (program file) must be prefixed with "../" (because of cd before grun)
+
+# usage: ./antlr_parsing_tree.sh grammar_name start_rule ./program args
+# the above usage is obsolte and has been hardcoded
+
+# out_dir="./out"
+# src_dir="./src"
+# grammar="$1"
+# grammar_file="$grammar.g4"
+# start_rule="$2"
+# program="$3"
+# shift
+# shift
+# shift
+# args="$@"
+
+# new usage: ./antlr_parsing_tree.sh ./program
+
+out_dir="./out"
+src_dir="./src"
+
+args="-gui"
+grammar="LExpr"
+grammar_file="$grammar.g4"
+start_rule="s"
+program="$1"
+
+# setup grammar in correct placement for grun
+cp $(echo "$src_dir/$grammar_file") $(echo "$out_dir/$grammar/")
+
+cd $out_dir
+grun $(echo "$grammar.$grammar $start_rule .$program $args")
